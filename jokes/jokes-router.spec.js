4533a5731490e557020a6f99b845877dcdac78da
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-15 10:47:07 -0800

    MVP: added test to jokes-router.spec.js to see if array of jokes is included in response; passing

--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -15,6 +15,23 @@ describe('jokes router', () => {
                 const token = newUser.body.token
                 const jokes = await request(server).get('/api/jokes/').set("authorization", token);
                 return expect(jokes.status).toBe(200);
-            });
+            }
+        );
+
+        it('should respond with jokes',
+            async () => {
+                const info = {
+                    username: `testingUser${new Date()}`,
+                    password: "testingPassword"
+                };
+
+                const newUser = await request(server).post('/api/auth/register').send(info);
+                const token = newUser.body.token
+                const jokes = await request(server).get('/api/jokes/').set("authorization", token);
+                const joke1 = jokes.body[0];
+                console.log("Joke 1: ", joke1.joke);
+                return expect(joke1).toHaveProperty("joke");
+            }
+        );
     })
 })
