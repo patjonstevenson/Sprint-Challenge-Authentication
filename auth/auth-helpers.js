7e1f5cae962e7ee945e8adc9c4e99fff169cd874
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-15 09:26:53 -0800

    wrote authenticate-middleware

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = {
     validateUser,
     getJwtToken
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
 
 const Users = require('./auth-model');
 const { validateUser, getJwtToken } = require('./auth-helpers');
